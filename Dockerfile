1ab96e7d4a452a9f5be12ab2c82ebbde18beda6d
Felipe Rivera <yf.rivera1851@uniandes.edu.co>
2025-05-04 13:03:21 -0500

    fixes data base

--- Dockerfile
+++ Dockerfile
@@ -9,17 +9,4 @@ COPY . .
 
 EXPOSE 5000
 
-ARG DB_HOST
-ARG DB_PORT
-ARG DB_NAME
-ARG DB_USER
-ARG DB_PASSWORD
-
-ENV DB_HOST=$DB_HOST \
-    DB_PORT=$DB_PORT \
-    DB_NAME=$DB_NAME \
-    DB_USER=$DB_USER \
-    DB_PASSWORD=$DB_PASSWORD \
-    FLASK_ENV=production
-
 CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "application:app"]
--- application.py
+++ application.py
@@ -42,16 +42,16 @@ def create_app(test_config=None):
     if test_config:
         app.config.update(test_config)
     else:
-        DB_HOST = os.getenv("DB_HOST", "localhost")
-        DB_PORT = os.getenv("DB_PORT", "5432")
-        DB_NAME = os.getenv("DB_NAME", "nombre_de_tu_bd")
-        DB_USER = os.getenv("DB_USER", "usuario")
-        DB_PASSWORD = os.getenv("DB_PASSWORD", "contraseña")
-
-        app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        database_url = os.getenv('DATABASE_URL')
+        if database_url:
+            app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+        else:
+            app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./test.db'
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
     # Routes
     @app.route('/blacklists', methods=['POST'])
@@ -99,8 +99,6 @@ def create_app(test_config=None):
 # ✅ Instancia global para Gunicorn
 app = create_app()
 
-# Solo ejecuta si corres python application.py directamente
+# Sólo ejecuta este bloque cuando corres directamente con Python
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(host='0.0.0.0', port=80, debug=False)
